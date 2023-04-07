64a8c8cac5b5e51a0aed7cef1f46e8772dc7aaea
YawKar <yawkarpov@gmail.com>
2023-04-07 19:51:19 +0300

    feat: add solution to the 17th exercise

---
+++ exercises/17.py
@@ -0,0 +1,9 @@
+#### 17. What is the result of the following expression? (★☆☆)
+import numpy as np
+
+print(0 * np.nan)
+print(np.nan == np.nan)
+print(np.inf > np.nan)
+print(np.nan - np.nan)
+print(np.nan in set([np.nan]))
+print(0.3 == 3 * 0.1)
